3b06427aa5cfcd73e4c75bf22c0b9cab6d149d42
zorobin04 <nggopinath04@gmail.com>
2026-04-13 10:46:53 +0530

    models added

---
+++ Room-Api/Room-Api/models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Room_Api.models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public ICollection<Booking> Bookings { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ Room-Api/Room-Api/models/BookingService.cs
@@ -0,0 +1,13 @@
+﻿namespace Room_Api.models
+{
+    public class BookingService
+    {
+        public int Id { get; set; }
+        public int BookingId { get; set; }
+        public Booking Booking { get; set; }
+        public int AmenityId { get; set; }
+        public Amenity Amenity { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
