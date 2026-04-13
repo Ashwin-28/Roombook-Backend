6e7d1748d11c16b8ae6f8c01f0ed7ddc322883dc
Subhiksha <shasubee0708@gmail.com>
2026-04-13 12:44:54 +0530

    changes in authresponseDTO

--- Room-Api/Room-Api/DTOs/AuthResponseDto.cs
+++ Room-Api/Room-Api/DTOs/AuthResponseDto.cs
@@ -5,5 +5,8 @@
     public string Token { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
-}
+        public DateTime Expiration { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
 }
